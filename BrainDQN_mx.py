9d8d1e90a4d0232ac6728c937fdd969fee135f19
Yao Zhou <zhouyao@mail2.sysu.edu.cn>
2016-11-15 21:07:34 +0800

    Update BrainDQN_mx.py

--- BrainDQN_mx.py
+++ BrainDQN_mx.py
@@ -10,6 +10,7 @@ import random
 from collections import deque 
 
 # Hyper Parameters:
+DUEL = True
 FRAME_PER_ACTION = 1
 GAMMA = 0.99 # decay rate of past observations
 OBSERVE = 100. # timesteps to observe before training
@@ -77,11 +78,56 @@ class BrainDQN:
             return Qvalue
         else:
             return loss
+        
+        
+      def gen_duel_sym(self, n_acts, batch_size, predict=False):
+        data = mx.sym.Variable('data')
+        yInput = mx.sym.Variable('yInput')
+        actionInput = mx.sym.Variable('actionInput')
+        conv1 = mx.sym.Convolution(data=data,kernel=(8,8),stride=(4,4),pad=(2,2),num_filter=32,name='conv1')
+        relu1 = mx.sym.Activation(data=conv1,act_type='relu',name='relu1')
+        pool1 = mx.sym.Pooling(data=relu1,kernel=(2,2),stride=(2,2),pool_type='max',name='pool1')
+        conv2 = mx.sym.Convolution(data=pool1,kernel=(4,4),stride=(2,2),pad=(1,1),num_filter=64,name='conv2')
+        relu2 = mx.sym.Activation(data=conv2,act_type='relu',name='relu2')
+        conv3 = mx.sym.Convolution(data=relu2,kernel=(3,3),stride=(1,1),pad=(1,1),num_filter=64,name='conv3')
+        relu3 = mx.sym.Activation(data=conv3,act_type='relu',name='relu3')
+        flat  = mx.sym.Flatten(data=relu3,NameError='flat')
+        fc1 = mx.sym.FullyConnected(data=flat, num_hidden=512,name='fc1')
 
+        val_stream = mx.sym.FullyConnected(data=fc1, num_hidden=512, name='val_stream_linear')
+        val_stream = mx.sym.Activation(data=val_stream, act_type='relu', name='val_stream_act')
+        val_stream = mx.sym.FullyConnected(data=val_stream, num_hidden=1, name='val_stream_res')
+
+        adv_stream = mx.sym.FullyConnected(data=fc1, num_hidden=512, name='adv_stream_linear')
+        adv_stream = mx.sym.Activation(data=adv_stream, act_type='relu', name='adv_stream_act')
+        adv_stream = mx.sym.FullyConnected(data=adv_stream, num_hidden=n_acts, name='adv_stream_res')
+        
+        sum = mx.sym.sum(data=adv_stream,axis=(1,), name='sum')
+        mean = sum / n_acts
+        mean = mx.sym.Reshape(data=mean, shape=(batch_size, 1), name='rs_mean')
+
+        sub_stream = mx.sym.broadcast_minus(adv_stream, mean)
+        Qvalue = mx.sym.broadcast_plus(sub_stream, val_stream)
+
+        temp=Qvalue*actionInput
+        coeff=mx.sym.sum(temp,axis=1,name='temp1')
+        output = (coeff - yInput)**2
+        loss=mx.sym.MakeLoss(output)
+        
+        if predict:
+            return Qvalue
+        else:
+            return loss
+        
+        
     def createQNetwork(self,bef_args=None,isTrain=True):
         if isTrain:
-            modQ = mx.mod.Module(symbol=self.sym(), data_names=('data','actionInput'), label_names=('yInput',), context=ctx)
             batch=BATCH_SIZE
+            if DUEL:
+                modQ = mx.mod.Module(symbol=self.gen_duel_sym(self.actions, batch), data_names=('data','actionInput'), label_names=('yInput',), context=ctx)
+            else:
+                modQ = mx.mod.Module(symbol=self.sym(), data_names=('data','actionInput'), label_names=('yInput',), context=ctx)
+                
             modQ.bind(data_shapes=[('data',(batch,4,80,80)),('actionInput',(batch,self.actions))],
                         label_shapes=[('yInput',(batch,))],
                         for_training=isTrain)
@@ -95,8 +141,12 @@ class BrainDQN:
                     'beta1': 0.5,
             })
         else:
-            modQ = mx.mod.Module(symbol=self.sym(predict=True), data_names=('data',), label_names=None, context=ctx)
             batch=1
+            if DUEL:
+                modQ = mx.mod.Module(symbol=self.gen_duel_sym(self.actions, batch, predict=True), data_names=('data',), label_names=None, context=ctx)
+            else:
+                modQ = mx.mod.Module(symbol=self.sym(predict=True), data_names=('data',), label_names=None, context=ctx)
+            
             modQ.bind(data_shapes=[('data',(batch,4,80,80))],
                         for_training=isTrain)
 
